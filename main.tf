9ff34638da4139a4e9c969c77ee959e8b271921f
ramanathan <ramanathan.tamilarasan@gmail.com>
2022-03-06 23:15:34 +0530

    terraform added tenant id

--- main.tf
+++ main.tf
@@ -1,21 +1,23 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~>2.31.1"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.31.1"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
+
+  tenant_id = "3b76748a-d23d-4d72-8c0e-ba387c03db72"
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "my_terraform_rg"
-    location = "sountcentralus"
-    tags = {
-        environment = "dev"
-        source = "Terraform"
-    }
+  name     = "my_terraform_rg"
+  location = "sountcentralus"
+  tags = {
+    environment = "dev"
+    source      = "Terraform"
+  }
 }
